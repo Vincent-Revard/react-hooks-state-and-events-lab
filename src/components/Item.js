28e9738ee20ea0164cab7eddf30eefda261b617e
Vincent Revard <Vincent.Revard@gmail.com>
2024-02-19 21:37:26 -0800

    Complete Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [addedCart, setAddedCart] = useState(false)
+
+  const handleAddCart = () => {
+    setAddedCart(!addedCart)
+  }
+
+  const liClass = addedCart ? "in-cart" : ""
+
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddCart}>{liClass? 'Remove From Cart': 'Add to Cart'}</button>
     </li>
   );
 }
